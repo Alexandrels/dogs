6c4c571614d8041e1cf32a4fa41b22ac34912dff
Alexandre <alexandre@mobilesys.com.br>
2020-03-03 18:10:24 -0300

    initial

---
+++ app/src/main/java/br/com/w2/dogs/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.w2.dogs.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.w2.dogs.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
