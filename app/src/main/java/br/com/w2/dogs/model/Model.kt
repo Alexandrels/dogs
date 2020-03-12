b9ab5ba0dcd35145f3dcf30984c992d0b1c1feb7
Alexandre <alexandre@mobilesys.com.br>
2020-03-12 17:15:06 -0300

    parei no viewModel

---
+++ app/src/main/java/br/com/w2/dogs/model/Model.kt
@@ -0,0 +1,10 @@
+package br.com.w2.dogs.model
+
+data class DogBreed(
+    val breedId: String?,
+    val dogBreed: String?,
+    val lifespan: String?,
+    val breedGroup: String?,
+    val breedFor: String?,
+    val imageUrl: String?
+)
---
+++ app/src/main/java/br/com/w2/dogs/view/DogsListAdapter.kt
@@ -0,0 +1,35 @@
+package br.com.w2.dogs.view
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import br.com.w2.dogs.R
+import br.com.w2.dogs.model.DogBreed
+import kotlinx.android.synthetic.main.item_dog.view.*
+
+class DogsListAdapter(val dogsList: ArrayList<DogBreed>) :
+    RecyclerView.Adapter<DogsListAdapter.DogViewHolder>() {
+    fun updateDogLIst(newDogList: List<DogBreed>) {
+        dogsList.clear()
+        dogsList.addAll(newDogList)
+        notifyDataSetChanged()
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DogViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.item_dog, parent, false)
+        return DogViewHolder(view)
+    }
+
+    override fun getItemCount() = dogsList.size
+
+    override fun onBindViewHolder(holder: DogViewHolder, position: Int) {
+        holder.view.name.text = dogsList[position].dogBreed
+        holder.view.lifespan.text = dogsList[position].lifespan
+    }
+
+
+    class DogViewHolder(var view: View) : RecyclerView.ViewHolder(view)
+}
